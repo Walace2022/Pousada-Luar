96e74cf073b5e02731dda93bbbc4fb9953cf1a00
Henrique Walace <henriquewalace@hotmail.com>
2024-05-24 15:39:59 -0300

    refactor: Adiciona construtor em Pessoa

--- Pousada Luar/Modelos/Pessoa.cs
+++ Pousada Luar/Modelos/Pessoa.cs
@@ -4,4 +4,10 @@ internal class Pessoa
 {
     public string Nome { get; set; }
     public string Sobrenome { get; set; }
+
+    public Pessoa(string nome, string sobrenome)
+    {
+        Nome = nome;
+        Sobrenome = sobrenome;
+    }   
 }
