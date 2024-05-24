be3ebe1cc97df19b132753449f32855e0b090d17
Henrique Walace <henriquewalace@hotmail.com>
2024-05-24 16:08:28 -0300

    feat: Adiciona instancias para teste de suíte, pessoa e cadastro de reservas

--- Pousada Luar/Program.cs
+++ Pousada Luar/Program.cs
@@ -1,1 +1,28 @@
 ﻿
+using Pousada_Luar.Modelos;
+
+Suite basica = new Suite("Basica", 2, 70.00M);
+Suite dupla = new Suite("Dupla", 4, 140.00M);
+Suite premium = new Suite("Premium", 8, 260.00M);
+
+Pessoa paulo = new Pessoa("Paulo", "Almeida");
+Pessoa andre = new Pessoa("Andre", "Barros");
+Pessoa maria = new Pessoa("Maria", "Freitas");
+
+List<Pessoa> hospedes = new List<Pessoa>() { paulo , andre ,maria};
+
+try
+{   
+    Reserva reservaFevereiro = new Reserva(hospedes, dupla, 7);
+    reservaFevereiro.DetalhesReserva();
+    Reserva reservaMarço = new Reserva(hospedes, premium, 10);
+    reservaMarço.DetalhesReserva();
+
+    //Teste de capacidade 
+    Reserva reservaJaneiro = new Reserva(hospedes, basica, 5);
+
+}
+catch(Exception e)
+{
+    Console.WriteLine($"Aconteceu um erro: {e.Message}");
+}
