e06f8d15c15d1a89db17927530d706729a94ba64
Henrique Walace <henriquewalace@hotmail.com>
2024-05-24 16:16:04 -0300

    test: Adiciona teste para zero hospedes

--- Pousada Luar/Program.cs
+++ Pousada Luar/Program.cs
@@ -2,8 +2,8 @@
 using Pousada_Luar.Modelos;
 
 Suite basica = new Suite("Basica", 2, 70.00M);
-Suite dupla = new Suite("Dupla", 4, 140.00M);
-Suite premium = new Suite("Premium", 8, 260.00M);
+Suite dupla = new Suite("Dupla", 4, 140.70M);
+Suite premium = new Suite("Premium", 8, 261.62M);
 
 Pessoa paulo = new Pessoa("Paulo", "Almeida");
 Pessoa andre = new Pessoa("Andre", "Barros");
@@ -19,10 +19,13 @@ try
     reservaMarço.DetalhesReserva();
 
     //Teste de capacidade 
-    Reserva reservaJaneiro = new Reserva(hospedes, basica, 5);
+    //Reserva reservaJaneiro = new Reserva(hospedes, basica, 5);
+
+    //Teste 0 hóspedes
+    //Reserva reservaJaneiro = new Reserva(new List<Pessoa>(), basica, 5);
 
 }
-catch(Exception e)
+catch (Exception e)
 {
     Console.WriteLine($"Aconteceu um erro: {e.Message}");
 }
