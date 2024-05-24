d31075dc4a24a7110ac22adefce5ea6d2b26c1ee
Henrique Walace <henriquewalace@hotmail.com>
2024-05-24 14:08:05 -0300

    feat: Cria modelo de Reserva

---
+++ Pousada Luar/Modelos/Reserva.cs
@@ -0,0 +1,51 @@
+﻿namespace Pousada_Luar.Modelos;
+
+internal class Reserva
+{
+    private List<Pessoa> Hospedes { get; set; }
+    private Suite Suite { get; set; }
+    public int DiasReservados { get; set; }
+
+    public Reserva(List<Pessoa> hospedes, Suite suite, int diasReservados)
+    {
+        CadastrarHospedes(hospedes);
+        CadastrarSuite(suite);
+        DiasReservados = diasReservados;
+    }
+
+    private void CadastrarHospedes(List<Pessoa> pessoas)
+    {
+        if (pessoas.Count == 0)
+        {
+            throw new ArgumentNullException("Não há hóspedes para serem cadastrados na reserva.");
+        }
+        Hospedes = pessoas;
+    }
+
+    private void CadastrarSuite(Suite suite)
+    {
+        if (suite == null)
+        {
+            throw new ArgumentNullException("Suite inválida ou não cadastrada.");
+        }
+        Suite = suite;
+
+    }
+
+    public int ObterQuantidadeHospedes()
+    {
+        return Hospedes.Count;
+    }
+
+    public decimal CalcularValorDiaria()
+    {
+        decimal valorTotal = DiasReservados * Suite.ValorDiaria;
+        if (DiasReservados >= 10)
+        {
+            return valorTotal * 0.9M;
+        }
+        return valorTotal;
+    }
+
+
+}
