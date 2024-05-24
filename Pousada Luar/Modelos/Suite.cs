ea7ed6e5b7d6813188ce3abf348af859adc31e64
Henrique Walace <henriquewalace@hotmail.com>
2024-05-24 15:37:12 -0300

    refactor: Adiciona Construtor em Suite

--- Pousada Luar/Modelos/Suite.cs
+++ Pousada Luar/Modelos/Suite.cs
@@ -5,5 +5,12 @@ internal class Suite
     public string TipoSuite { get; set; }
     public int Capacidade { get; set; }
     public decimal ValorDiaria { get; set; }
-    
+
+    public Suite(string tipoSuite, int capacidade, decimal valorDiaria)
+    {
+        TipoSuite = tipoSuite;
+        Capacidade = capacidade;
+        ValorDiaria = valorDiaria;
+    }
+
 }
