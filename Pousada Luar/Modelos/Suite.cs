44c4d6c3f77048e90464d3e7c7a6268d9d091eb3
Henrique Walace <henriquewalace@hotmail.com>
2024-05-24 13:55:04 -0300

    feat: Cria modelo de Suite

---
+++ Pousada Luar/Modelos/Suite.cs
@@ -0,0 +1,9 @@
+﻿namespace Pousada_Luar.Modelos;
+
+internal class Suite
+{
+    public string TipoSuite { get; set; }
+    public int Capacidade { get; set; }
+    public decimal ValorDiaria { get; set; }
+    
+}
